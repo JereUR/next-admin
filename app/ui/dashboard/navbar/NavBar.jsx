0aa27c23167e3dad524621d586b0489a2b2abf50
JereUR <jeremias.jdv@gmail.com>
2024-02-14 19:14:51 -0300

    feat(Root): Navbar elements and styles added

--- app/ui/dashboard/navbar/NavBar.jsx
+++ app/ui/dashboard/navbar/NavBar.jsx
@@ -1,7 +1,38 @@
-import React from 'react'
+'use client'
+
+import { usePathname } from 'next/navigation'
+import {
+  MdNotifications,
+  MdOutlineChat,
+  MdPublic,
+  MdSearch
+} from 'react-icons/md'
 
 const NavBar = () => {
-  return <div>NavBar</div>
+  const pathname = usePathname()
+
+  return (
+    <div className="flex items-center justify-between p-5 rounded-xl bg-gray-800">
+      <div className="text-gray-300 font-bold capitalize">
+        {pathname.split('/').pop()}
+      </div>
+      <div className="flex items-center gap-5">
+        <div className="flex items-center gap-2 bg-gray-700 p-2 rounded-xl">
+          <MdSearch />
+          <input
+            className="bg-transparent border-none text-white"
+            type="text"
+            placeholder="Search..."
+          />
+        </div>
+        <div className="flex gap-5">
+          <MdOutlineChat size={20} />
+          <MdNotifications size={20} />
+          <MdPublic size={20} />
+        </div>
+      </div>
+    </div>
+  )
 }
 
 export default NavBar
