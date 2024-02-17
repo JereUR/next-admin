2f8e374dcc893d181bc4f0c7a1946b68554c9856
JereUR <jeremias.jdv@gmail.com>
2024-02-17 18:54:35 -0300

    feat(Root): Min-height attribute added

--- app/dashboard/layout.jsx
+++ app/dashboard/layout.jsx
@@ -5,7 +5,7 @@ import SideBar from '../ui/dashboard/sidebar/SideBar'
 const Layout = ({ children }) => {
   return (
     <div className="flex">
-      <div className="w-1/5 bg-gray-800 p-5">
+      <div className="w-1/5 bg-gray-800 p-5 min-h-screen">
         <SideBar />
       </div>
       <div className="w-4/5 p-5">
