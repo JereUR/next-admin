0b582f9b594485db19b7cfbb9f191871aa3524f6
JereUR <jeremias.jdv@gmail.com>
2024-02-17 18:55:01 -0300

    feat(Root): Page for add new product created and styled

---
+++ app/dashboard/products/add/page.jsx
@@ -0,0 +1,36 @@
+import React from 'react'
+
+const addProductPage = () => {
+  return (
+    <div className="bg-gray-800 p-5 rounded-lg mt-5">
+      <form action="" className="form-product flex flex-wrap justify-between">
+        <input type="text" placeholder="Title" name="title" required />
+        <select name="cat" id="cat">
+          <option value="general">Choose a Category</option>
+          <option value="kitchen">Kitchen</option>
+          <option value="phone">Phone</option>
+          <option value="computer">Computer</option>
+        </select>
+        <input type="number" placeholder="Price" name="price" />
+        <input type="number" placeholder="Stock" name="stock" />
+        <input type="text" placeholder="Color" name="color" />
+        <input type="text" placeholder="Size" name="size" />
+        <textarea
+          className="w-full"
+          name="description"
+          placeholder="Description"
+          id="desc"
+          rows="16"
+        ></textarea>
+        <button
+          className="w-full p-7 bg-teal-600 text-white rounded-sm cursor-pointer"
+          type="submit"
+        >
+          Submit
+        </button>
+      </form>
+    </div>
+  )
+}
+
+export default addProductPage
