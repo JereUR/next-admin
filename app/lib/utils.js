c332d4520e5b00d38edf9484ed011d2749b4d6f4
JereUR <jeremias.jdv@gmail.com>
2024-02-20 22:06:11 -0300

    feat(Root): Connection to MongoDB database

--- app/lib/utils.js
+++ app/lib/utils.js
@@ -1,12 +1,17 @@
-import mongoose from 'mongoose'
+const mongoose = require('mongoose')
 
 export const connectToDB = async () => {
   const connection = {}
   try {
     if (connection.isConnected) return
-    const db = await mongoose.connectToDB(process.env.MONGO_URL)
-    connection.isConnected = db.connection[0].readyState
+    const db = await mongoose.connect(process.env.MONGO_URL, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true
+    })
+    console.log(connection)
+    connection.isConnected = db.connections[0].readyState
   } catch (error) {
+    console.log(error)
     throw new Error(error.message)
   }
 }
