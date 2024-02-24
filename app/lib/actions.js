55fdec853c14e1e850710d46405ea052da67cd3a
JereUR <jeremias.jdv@gmail.com>
2024-02-23 21:27:20 -0300

    feat(Root): Update and delete actions for user and product created

--- app/lib/actions.js
+++ app/lib/actions.js
@@ -36,8 +36,39 @@ export const addUser = async (formData) => {
   redirect('/dashboard/users')
 }
 
+export const updateUser = async (formData) => {
+  const { id, username, email, password, phone, address, isAdmin, isActive } =
+    Object.fromEntries(formData)
+
+  try {
+    connectToDB()
+    const updateFields = {
+      username,
+      email,
+      password,
+      phone,
+      address,
+      isAdmin,
+      isActive
+    }
+
+    Object.keys(updateFields).forEach(
+      (key) =>
+        (updateFields[key] === '' || undefined) && delete updateFields[key]
+    )
+
+    await User.findByIdAndUpdate(id, updateFields)
+  } catch (error) {
+    console.log(error)
+    throw new Error('Failed to update user!')
+  }
+
+  revalidatePath('/dashboard/users')
+  redirect('/dashboard/users')
+}
+
 export const addProduct = async (formData) => {
-  const { title, description, price, stock, color, size } =
+  const { title, category, description, price, stock, color, size } =
     Object.fromEntries(formData)
 
   try {
@@ -45,6 +76,7 @@ export const addProduct = async (formData) => {
 
     const newProduct = new Product({
       title,
+      category,
       description,
       price,
       stock,
@@ -60,4 +92,65 @@ export const addProduct = async (formData) => {
 
   revalidatePath('/dashboard/products')
   redirect('/dashboard/products')
+}
+
+export const updateProduct = async (formData) => {
+  const { id, title, category, description, price, stock, color, size } =
+    Object.fromEntries(formData)
+
+  try {
+    connectToDB()
+    const updateFields = {
+      title,
+      category,
+      description,
+      price,
+      stock,
+      color,
+      size
+    }
+
+    Object.keys(updateFields).forEach(
+      (key) =>
+        (updateFields[key] === '' || undefined) && delete updateFields[key]
+    )
+
+    await Product.findByIdAndUpdate(id, updateFields)
+  } catch (error) {
+    console.log(error)
+    throw new Error('Failed to update product!')
+  }
+
+  revalidatePath('/dashboard/products')
+  redirect('/dashboard/products')
+}
+
+export const deleteProduct = async (formData) => {
+  const { id } = Object.fromEntries(formData)
+
+  try {
+    connectToDB()
+
+    await Product.findByIdAndDelete(id)
+  } catch (error) {
+    console.log(error)
+    throw new Error('Failed to delete product!')
+  }
+
+  revalidatePath('/dashboard/products')
+}
+
+export const deleteUser = async (formData) => {
+  const { id } = Object.fromEntries(formData)
+
+  try {
+    connectToDB()
+
+    await User.findByIdAndDelete(id)
+  } catch (error) {
+    console.log(error)
+    throw new Error('Failed to delete user!')
+  }
+
+  revalidatePath('/dashboard/users')
 }
