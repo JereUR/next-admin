150fd72f8d8b72da167507a0fa481f002ea11f17
JereUR <jeremias.jdv@gmail.com>
2024-02-29 21:23:38 -0300

    feat(Root): Test photo from local db

--- app/dashboard/routines/page.jsx
+++ app/dashboard/routines/page.jsx
@@ -1,5 +1,19 @@
+import Image from 'next/image'
+
+import localDB from '../../localdb/db_local.json'
+
 const RoutinesPage = () => {
-  return <div>RoutinesPage</div>
+  console.log(localDB.exercises['Abdominales y lumbares'][0].photo)
+  return (
+    <div>
+      <Image
+        src={localDB.exercises['Abdominales y lumbares'][0].photo}
+        width={40}
+        height={40}
+        alt={localDB.exercises['Abdominales y lumbares'][0].exercise}
+      />
+    </div>
+  )
 }
 
 export default RoutinesPage
