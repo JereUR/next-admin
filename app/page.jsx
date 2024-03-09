48b282165268ef0f03422de51a2870bd37f2102f
JereUR <jeremias.jdv@gmail.com>
2024-03-09 19:06:39 -0300

    feat(Root): link to dashboard added

--- app/page.jsx
+++ app/page.jsx
@@ -1,6 +1,15 @@
+import Link from 'next/link'
+
 const Homepage = () => {
   return (
-    <div>Homepage</div>
+    <div>
+      <Link
+        className="p-4 flex items-center gap-2 my-1 mx-0 rounded-lg hover:bg-gray-700 "
+        href="/dashboard"
+      >
+        Ir a Dashboard
+      </Link>
+    </div>
   )
 }
 
