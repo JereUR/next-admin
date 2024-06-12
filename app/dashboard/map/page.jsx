c1e139cf3c2a35f8c4f36a05880691dff7f6b067
JereUR <jeremias.jdv@gmail.com>
2024-06-11 22:59:15 -0300

    feat: Map view component and page added

---
+++ app/dashboard/map/page.jsx
@@ -0,0 +1,12 @@
+import MapView from '@/app/ui/dashboard/map/MapView'
+import React from 'react'
+
+const MapPage = () => {
+  return (
+    <div className="m-10">
+      <MapView />
+    </div>
+  )
+}
+
+export default MapPage
---
+++ app/ui/dashboard/map/MapView.jsx
@@ -0,0 +1,174 @@
+'use client'
+
+import { Icon, divIcon, point } from 'leaflet'
+import 'leaflet/dist/leaflet.css'
+import { useRef, useState } from 'react'
+import { MapContainer, Marker, Popup, TileLayer } from 'react-leaflet'
+import MarkerClusterGroup from 'react-leaflet-cluster'
+import useGeolation from './useGeolation'
+import { GiGlobe } from 'react-icons/gi'
+import Autosuggest from 'react-autosuggest'
+import 'tailwindcss/tailwind.css'
+
+const MapView = () => {
+  const mapRef = useRef()
+  const location = useGeolation()
+  const ZOOM_LEVEL = 13
+  const [searchAddress, setSearchAddress] = useState('')
+  const [searchSuggestions, setSearchSuggestions] = useState([])
+  const [searchResult, setSearchResult] = useState(null)
+
+  const markers = [
+    { geocode: [48.86, 2.3522], popup: 'Hello, I am pop up 1' },
+    { geocode: [48.85, 2.3522], popup: 'Hello, I am pop up 2' },
+    { geocode: [48.855, 2.34], popup: 'Hello, I am pop up 3' }
+  ]
+
+  const customIcon = new Icon({
+    iconUrl: 'https://cdn-icons-png.flaticon.com/512/9561/9561845.png',
+    iconSize: [38, 38]
+  })
+
+  const homeIcon = new Icon({
+    iconUrl: 'https://cdn-icons-png.flaticon.com/512/7757/7757494.png',
+    iconSize: [38, 38]
+  })
+
+  const createCustomClusterIcon = (cluster) => {
+    return new divIcon({
+      html: `<div class='h-12 w-12 rounded-full bg-white transform translate-x-[-50%] translate-y-[-50%] flex justify-center items-center font-[900] text-xl text-black'>${cluster.getChildCount()}</div>`,
+      className: 'custom-marker-cluster',
+      iconSize: point(33, 33, true)
+    })
+  }
+
+  const showMyLocation = () => {
+    if (location.loaded && !location.error) {
+      const { lat, lng } = location.coords
+      mapRef.current.flyTo([lat, lng], ZOOM_LEVEL, {
+        animate: true,
+        duration: 1.5
+      })
+    } else {
+      alert(location.error.message)
+    }
+  }
+
+  const handleSearch = async (value) => {
+    const response = await fetch(
+      `https://nominatim.openstreetmap.org/search?format=json&q=${value}`
+    )
+    const results = await response.json()
+    setSearchSuggestions(results)
+  }
+
+  const onSuggestionSelected = (event, { suggestion }) => {
+    const { lat, lon } = suggestion
+    setSearchResult([lat, lon])
+    mapRef.current.flyTo([lat, lon], ZOOM_LEVEL, {
+      animate: true,
+      duration: 1.5
+    })
+  }
+
+  const onSuggestionsFetchRequested = ({ value }) => {
+    handleSearch(value)
+  }
+
+  const onSuggestionsClearRequested = () => {
+    setSearchSuggestions([])
+  }
+
+  const getSuggestionValue = (suggestion) => suggestion.display_name
+
+  const renderSuggestion = (suggestion) => (
+    <div className="p-2 cursor-pointer hover:bg-gray-200">
+      {suggestion.display_name}
+    </div>
+  )
+
+  const handleMarkerDragEnd = (event) => {
+    const marker = event.target
+    const position = marker.getLatLng()
+    setSearchResult([position.lat, position.lng])
+  }
+
+  console.log(searchResult)
+
+  return (
+    <div>
+      <div className="relative my-4">
+        <Autosuggest
+          suggestions={searchSuggestions}
+          onSuggestionsFetchRequested={onSuggestionsFetchRequested}
+          onSuggestionsClearRequested={onSuggestionsClearRequested}
+          getSuggestionValue={getSuggestionValue}
+          renderSuggestion={renderSuggestion}
+          inputProps={{
+            value: searchAddress,
+            onChange: (e, { newValue }) => setSearchAddress(newValue),
+            className: 'p-2 border border-gray-300 rounded text-black',
+            placeholder: 'Enter address'
+          }}
+          onSuggestionSelected={onSuggestionSelected}
+          theme={{
+            suggestionsContainer:
+              'absolute bg-white shadow-lg z-20 w-full text-black',
+            suggestionsList: 'm-0 p-0 list-none',
+            suggestion: '',
+            suggestionHighlighted: 'bg-gray-300'
+          }}
+        />
+      </div>
+      <MapContainer
+        center={[48.8566, 2.3522]}
+        zoom={ZOOM_LEVEL}
+        className="h-screen z-10"
+        ref={mapRef}
+      >
+        <TileLayer
+          attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+          url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+        />
+        <MarkerClusterGroup
+          chunkedLoading /*performance*/
+          iconCreateFunction={createCustomClusterIcon}
+        >
+          {markers.map((marker, index) => (
+            <Marker key={index} position={marker.geocode} icon={customIcon}>
+              <Popup>{marker.popup}</Popup>
+            </Marker>
+          ))}
+          {location.loaded && !location.error && (
+            <Marker
+              icon={homeIcon}
+              position={[location.coords.lat, location.coords.lng]}
+            ></Marker>
+          )}
+          {searchResult && (
+            <Marker
+              icon={customIcon}
+              position={searchResult}
+              draggable={true}
+              eventHandlers={{ dragend: handleMarkerDragEnd }}
+            >
+              <Popup>Search Result</Popup>
+            </Marker>
+          )}
+        </MarkerClusterGroup>
+      </MapContainer>
+      <div className="my-4 w-[10vw]">
+        <div className="flex flex-col justify-center">
+          <button
+            className="flex gap-2 items-center justify-center p-2 bg-red-600 text-white"
+            onClick={showMyLocation}
+          >
+            Locate Me <GiGlobe />
+          </button>
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default MapView
