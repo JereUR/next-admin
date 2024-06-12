30ba64175edea1da462e3897b09d359f2e48ea0c
JereUR <jeremias.jdv@gmail.com>
2024-06-11 22:58:33 -0300

    feat: Geolocation card created (redirect to map section)

--- app/dashboard/page.jsx
+++ app/dashboard/page.jsx
@@ -1,4 +1,5 @@
 import Card from '../ui/dashboard/card/Card'
+import CardGeolocation from '../ui/dashboard/card/CardGeolocation'
 import Chart from '../ui/dashboard/chart/Chart'
 import RightBar from '../ui/dashboard/rightbar/RightBar'
 import Transactions from '../ui/dashboard/transactions/Transactions'
@@ -8,7 +9,7 @@ const DashboardPage = () => {
     <div className="flex gap-5 mt-5">
       <div className="w-3/4 flex flex-col gap-5">
         <div className="flex gap-5 justify-between">
-          <Card />
+          <CardGeolocation />
           <Card />
           <Card />
         </div>
