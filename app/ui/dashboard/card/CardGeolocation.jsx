2bca3bf70926c11d1f0ba5899576e8ce986c4179
JereUR <jeremias.jdv@gmail.com>
2024-06-11 23:01:04 -0300

    feat: Styles added

--- app/ui/dashboard/card/CardGeolocation.jsx
+++ app/ui/dashboard/card/CardGeolocation.jsx
@@ -2,20 +2,14 @@ import Link from 'next/link'
 import { MdLocationCity } from 'react-icons/md'
 
 const CardGeolocation = () => {
-  const porc = 12
   return (
     <div className="bg-gray-800 p-5 rounded-lg flex gap-5 cursor-pointer w-full hover:bg-gray-700">
       <MdLocationCity size={24} />
       <div className="flex flex-col">
         <span>Geolocation</span>
-        <span className="text-2xl font-medium">10.273</span>
-        <span className="text-sm font-light">
-          <span className={`${porc > 0 ? 'text-lime-500' : 'text-red-500'}`}>
-            {porc}%
-          </span>{' '}
-          more than previous week
-        </span>
-        <Link href="/dashboard/map">See my location</Link>
+        <Link href="/dashboard/map" className="p-2 bg-purple-600 text-white">
+          See map
+        </Link>
       </div>
     </div>
   )
